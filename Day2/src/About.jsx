acec156dfb577392236db076d15f6390c8d37275
JameerHussain <150135083+jameerHuss@users.noreply.github.com>
2024-07-30 13:52:51 +0530

    Add files via upload

---
+++ Day2/src/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './About.css';
+import img2 from './Assets/img2.jpg'
+function About() {
+  return (
+    <>
+    <div className="about">
+      <h2>About Us</h2>
+      <p className='paras'>Welcome to the Toy Store! We are dedicated to providing the best toys for kids of all ages. Our mission is to bring joy and learning through our carefully curated selection of toys. Whether you're looking for educational toys, fun games, or creative playthings, we have something for everyone. Thank you for visiting us!</p>
+    </div>
+      {/* <img src={img2}></img> */}
+      </>
+  );
+}
+
+export default About;
