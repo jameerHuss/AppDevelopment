1795c235abb319f2372693cd032703e649a13d0a
JameerHussain <150135083+jameerHuss@users.noreply.github.com>
2024-08-07 23:34:50 +0530

    Delete Day11/ToyStore (1)/ToyStore directory

--- Day11/ToyStore (1)/ToyStore/src/main/java/com/toystore/ToyStore/Repository/ToyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.toystore.ToyStore.Repository;
-
-import com.toystore.ToyStore.Model.Toy;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ToyRepository extends JpaRepository<Toy, Long> {
-}
